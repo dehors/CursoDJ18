294e2a10a212dbd0b4e056a84ad9a317cab2d488
slam24 <luismartinez242011@gmail.com>
2016-02-15 22:00:14 -0600

    feat: Url & View

--- Proyecto18/urls.py
+++ Proyecto18/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'Proyecto18.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('app.urls')),
+    url(r'^', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
