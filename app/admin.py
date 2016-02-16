8a7b5f508fbc8092812b8dd4e59e19cff6e96984
slam24 <luismartinez242011@gmail.com>
2016-02-15 23:05:06 -0600

    feat: add model to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SingUp
+
+class PostModelSingUp(admin.ModelAdmin):
+	list_display = ["email","updated","timestamp"] # campos a mostrar
+	list_display_links = ["updated"] #campo link para editar
+	list_editable = ["email"]#campo editable
+	list_filter = ["updated","timestamp"]#filtros de fechas en la parte derecha
+
+	search_fields = ["email"]#buscador
+	class Meta:
+		model = SingUp
+
+admin.site.register(SingUp, PostModelSingUp)
