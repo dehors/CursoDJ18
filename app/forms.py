1f0b860e39e8b4d86ac08233dac320788bd5e9d9
slam24 <luismartinez242011@gmail.com>
2016-02-17 19:51:26 -0600

    feat: Form Validate

--- app/forms.py
+++ app/forms.py
@@ -8,4 +8,18 @@ class SingUpForm(forms.ModelForm):
 			"email",
 			"full_name",
 		]
-		exclude = ["full_name"]
+		#exclude = ["full_name"]
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		email_base, provider = email.split("@")
+		domain, extension = provider.split('.')
+		if not domain == 'USC':
+			raise forms.ValidationError("ingrese un correo de USC")
+		if not extension == "edu":
+			raise forms.ValidationError("Porfavor ingrese una direccion de correo valida ")
+		return email
+
+	def clean_full_name(self):
+		full_name = self.cleaned_data.get('full_name')
+		return full_name
