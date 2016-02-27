450aa7f05c21a40809612662491cdd31b4436c9a
slam24 <luismartinez242011@gmail.com>
2016-02-27 10:36:49 -0600

    feat: add view & url to project

--- Proyecto18/urls.py
+++ Proyecto18/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', include('app.urls')),
     url(r'^', include('app.urls')),
+    url(r'^about', 'Proyecto18.views.about', name='about'),
     url(r'^admin/', include(admin.site.urls)),
 ]
